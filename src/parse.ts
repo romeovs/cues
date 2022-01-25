1388289d92bdd5305e2372ed7a5440008dd31681
Romeo Van Snick <romeovs@gmail.com>
2022-01-25 22:11:52 +1100

    Add parse helper

---
+++ src/parse.ts
@@ -0,0 +1,40 @@
+import { Cue } from "./cue"
+import * as timestamp from "./timestamp"
+
+export type File = {
+	type: "webvtt" | "srt"
+	cues: Cue[]
+}
+
+const WEBVTT = "WEBVTT"
+
+export function parse(content: string): File {
+	const groups = content.split("\n\n")
+
+	const vtt = lines[0].toUpperCase() === WEBVTT
+	const type = vtt ? "webvtt" : "srt"
+
+	const cues = groups.map(parseCue).filter(isCue)
+
+	return { file, cues }
+}
+
+function parseCue(content: string): Cue | null {
+	const lines = content.split("\n")
+	if (lines[0].toUpperCase() === WEBVTT || lines.length !== 3) {
+		return null
+	}
+
+	const [start, end] = lines[1].split(" --> ")
+
+	return {
+		id: lines[0],
+		text: lines[2],
+		start: timestamp.parse(start),
+		end: timestamp.parse(end),
+	}
+}
+
+function isCue(cue: Cue | null): cue is Cue {
+	return cue !== null
+}
